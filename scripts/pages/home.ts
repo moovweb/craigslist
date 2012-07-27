423ba6fa9b0820cb5f9733bd37c953e0722e1e57
Michael JL Catlin <malrase@gmail.com>
2012-07-26 17:42:53 -0700

    CSS selectors in some places

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,4 +1,4 @@
-$("/html/body") {
+$$("body") {
   # Adding a class for page-specific styling
   add_class("_home")
 
@@ -10,45 +10,37 @@ $("/html/body") {
   remove(".//span[@id='copy']")
 
   # Changing tables so they are comprised of divs and spans
-  $(".//table") {
-    name("div")
-    attribute("class", "oldtable")
-    $("./tr") {
-      name("div")
-      add_class("_" + index() + "row")
-      $("td|th") {
-        name("span")
-      }
-    }
-  }
+  table_dump(".//table")
 
   # Removing the calendar
   remove(".//div[@summary='calendar']")
-
-  $(".//div[@id='main']") {
-
+  
+  $$("#main") {
+    
     # Turning the list of items into an accordion using Uranium (see http://uraniumjs.com)
-    $(".//div[@id='res']/h4") {
+    $$("#res > h4") {
       add_class("_category")
       # add span for button icon
-      $("./a"){
+      $$("a") {
         insert_bottom("span"){
-          attribute("class", "_toggler_subcategory")
+          attribute("class", "_icon_subcategory")
         }
       }
     }
-
+    
     # Adding attributes for Uranium widget 
+
+    # Using XPath to take advantage of the "not" function
     $(".//div[@class='col' and not(@id='res')]") {
       attribute("data-ur-set", "toggler")
-      $(".//h4") {
+      $$("h4") {
         attributes(class: "_category", 
                    data-ur-toggler-component: "button")
 
         # Wrapping any "naked" text in a div - mainly for the "Personals" tab
         wrap_text_children('div') {
-          insert("span") {
-            attribute("class", "_toggler_btn_icon")
+          insert("span", class: "_icon_category") {
+            #attribute("class", "_toggler_btn_icon")
           }
         }
 
@@ -57,66 +49,72 @@ $("/html/body") {
         insert_top("div", $txt) {
           # Adding a span to incorporate the button for the non-Personals categories
           insert("span") {
-            attribute("class", "_toggler_btn_icon")
+            attribute("class", "_icon_category")
           }
         }
-        $("./a") {
+        $$("a") {
           wrap("li") {
             move_to("../../div[@class='cats']/ul[1]", "top")
           }
         }
       }
-      $(".//ul") {
+      $$("ul") {
         attribute("data-ur-toggler-component", "content")
-        $("./li") {
+        $$("> li") {
           attribute("class", "_subcategory")
           # add span for button icon
-          $("./a"){
+          $$("> a"){
             insert_bottom("span"){
-              attribute("class", "_toggler_subcategory")
+              attribute("class", "_icon_subcategory")
             }
           }
         }
       }
     }
+
     # Removing the extra div above the Personals category
     $(".//div[@id='ppp']/h4/div[1]") {
       remove()
     }
   }
-
+  
 	# Implementing an accordion for the location lists, too
-  $(".//span[@id='rightbar']") {
-    $("./ul/li") {
+  $$("#rightbar") {
+    $$("> ul > li") {
       attribute("data-ur-set", "toggler")
-      $(".//h5") {
+      $$("h5") {
         attributes(class: "_category", 
                    data-ur-toggler-component: "button")
         wrap_text_children("div")
         # add span for button icon
         $("./div[1]"){
           insert("span"){
-            attribute("class", "_toggler_btn_icon")
+            attribute("class", "_icon_category")
           }
         }
       }
-      $(".//ul") {
+      $$("ul") {
         attribute("data-ur-toggler-component", "content")
-        $("./li") {
+        $$("> li") {
           attribute("class", "_subcategory")
+          $$("> a") {
+            insert_bottom("span") {
+              attribute("class", "_toggler_subcategory")
+            }
+          }
         }
       }
     }
   }
   # Making the header with another Uranium widget (tabs)
-  $(".//span[@id='leftbar']") {
+  $$("#leftbar") {
     attributes(data-ur-set: "tabs", 
                data-ur-closeable: "true")
     wrap_text_children("span") {
       move_to("//span[@id='rightbar']")
     }
     # Adding the logo icons for all the tabs 
-    $("./div[@id='logo']") {
+    $$("#logo") {
       insert("div", class: "_icons_bar") {
         insert("div", class: "icons-account", 
                       data-ur-tab-id: "account", 
@@ -131,15 +129,15 @@ $("/html/body") {
     }
 
     # Defining the content that will appear when the corresponding icon buttons are clicked
-    $("./ul[@id='postlks']") {
+    $$("ul#postlks") {
       attributes(data-ur-tabs-component: "content", 
                  data-ur-tab-id: "account",
                  data-ur-state: "disabled")
-      $("./li/a") {
+      $$("> li > a") {
         attribute("class", "_header_bar")
       }
     }
-    $("./form[@id='search']") {
+    $$("form#search") {
       attributes(data-ur-tabs-component: "content", 
                  data-ur-tab-id: "search",
                  data-ur-state: "disabled")
@@ -150,7 +148,7 @@ $("/html/body") {
       move_here("../ul[@id='leftlinks']")
       move_here("../div[@id='calttl']")
       move_here("../p[@id='sflinks']")
-      $(".//a") {
+      $$("a") {
         attribute("class", "_header_bar")
       }
     }
